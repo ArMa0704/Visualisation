d1614fe74b8eb01f612b2af9ccfffdc62f725685
@20222144 <s.vesely@student.tue.nl>
2024-01-31 19:36:06 +0100

    cleaning and data work

--- Test App/config/data.py
+++ Test App/config/data.py
@@ -1,10 +1,172 @@
 import pandas as pd
-
+import numpy as np
+import scipy.stats as stats
 def load_data():
 
     # Load dataset into a DataFrame
     df = pd.read_csv("Data/Raw/all_data.csv")
 
     # TODO : data preprocessing
+    def get_column_details(df, column):
+        print("Details of", column, "column")
 
+        # DataType of column
+        print("\nDataType: ", df[column].dtype)
+
+        # Check if null values are present
+        count_null = df[column].isnull().sum()
+        if count_null == 0:
+            print("\nThere are no null values")
+        elif count_null > 0:
+            print("\nThere are ", count_null, " null values")
+
+        # Get Number of Unique Values
+        print("\nNumber of Unique Values: ", df[column].nunique())
+
+        # Get Distribution of Column
+        print("\nDistribution of column:\n")
+        print(df[column].value_counts())
+
+    def fill_missing_with_group_mode(df, groupby, column):
+        print("\nNo. of missing values before filling with group mode:", df[column].isnull().sum())
+
+        mode_per_group = df.groupby(groupby)[column].transform(lambda x: x.mode().iat[0])
+        df[column] = df[column].fillna(mode_per_group)
+
+        print("\nNo. of missing values after filling with group mode:", df[column].isnull().sum())
+
+    def clean_categorical_field(df, groupby, column, replace_value=None):
+        print("\n-----------------------------------------------------")
+        print("\nCleaning steps ")
+
+        if replace_value != None:
+            df[column] = df[column].replace(replace_value, np.nan)
+            print(f"\nGarbage value {replace_value} is replaced with np.nan")
+
+        fill_missing_with_group_mode(df, groupby, column)
+
+    def fix_inconsistent_values(df, groupby, column):
+        print("\nExisting Min, Max Values:", df[column].apply([min, max]), sep='\n', end='\n')
+
+        df_dropped = df[df[column].notna()].groupby(groupby)[column].apply(list)
+        x, y = df_dropped.apply(lambda x: stats.mode(x)).apply([min, max])
+        mini, maxi = x[0], y[0]
+
+        # assign Wrong Values to NaN
+        col = df[column].apply(lambda x: np.NaN if ((x < mini) | (x > maxi) | (x < 0)) else x)
+
+        # fill with local mode
+        mode_by_group = df.groupby(groupby)[column].transform(lambda x: x.mode()[0] if not x.mode().empty else np.NaN)
+        df[column] = col.fillna(mode_by_group)
+        df[column].fillna(df[column].mean(), inplace=True)
+
+        print("\nAfter Cleaning Min, Max Values:", df[column].apply([min, max]), sep='\n', end='\n')
+
+    def clean_numerical_field(df, groupby, column, strip=None, datatype=None, replace_value=None):
+        print("\n-----------------------------------------------------")
+        print("\nCleaning steps ")
+
+        if replace_value != None:
+            df[column] = df[column].replace(replace_value, np.nan)
+
+        if df[column].dtype == object and strip is not None:
+            df[column] = df[column].str.strip(strip)
+
+        if datatype is not None:
+            df[column] = df[column].astype(datatype)
+        fix_inconsistent_values(df, groupby, column)
+
+    def Month_Converter(val):
+        if pd.notnull(val):
+            years = int(val.split(' ')[0])
+            month = int(val.split(' ')[3])
+            return (years * 12) + month
+        else:
+            return val
+
+    features_to_be_num = ['Num_of_Loan']
+    for feature in features_to_be_num:
+        df[feature] = df[feature].str.extract('(\d+)').astype(int)
+
+    features_to_be_num = ['Outstanding_Debt', 'Annual_Income', 'Amount_invested_monthly', 'Monthly_Balance']
+    for feature in features_to_be_num:
+        df[feature] = df[feature].str.extract(r'(\d+\.\d+)').astype(float)
+
+    column_name = 'Name'
+    group_by = 'Customer_ID'
+    print('name')
+    clean_categorical_field(df, group_by, column_name)
+
+    column_name = 'SSN'
+    group_by = 'Customer_ID'
+    garbage_value = '#F%$D@*&8'
+    print('ssn')
+    clean_categorical_field(df, group_by, column_name, garbage_value)
+
+    column_name = 'Payment_Behaviour'
+    group_by = 'Customer_ID'
+    garbage_value = '!@9#%8'
+    print('payment_behaviour')
+    clean_categorical_field(df, group_by, column_name, garbage_value)
+
+    column_name = 'Age'
+    group_by = 'Customer_ID'
+    print('age')
+    clean_numerical_field(df, group_by, column_name, strip='_', datatype='int')
+
+    column_name = 'Annual_Income'
+    group_by = 'Customer_ID'
+    print('annual income')
+    clean_numerical_field(df, group_by, column_name, strip='_', datatype='float')
+
+    column_name = 'Monthly_Inhand_Salary'
+    group_by = 'Customer_ID'
+    print('monthly inhand salry')
+    clean_numerical_field(df, group_by, column_name)
+
+    column_name = 'Num_Bank_Accounts'
+    group_by = 'Customer_ID'
+    print('num bank accounts')
+    clean_numerical_field(df, group_by, column_name)
+
+    column_name = 'Num_Credit_Card'
+    group_by = 'Customer_ID'
+    print('num credit cards')
+    clean_numerical_field(df, group_by, column_name)
+
+    column_name = 'Interest_Rate'
+    group_by = 'Customer_ID'
+    print('interest rate')
+    clean_numerical_field(df, group_by, column_name)
+
+    column_name = 'Num_of_Loan'
+    group_by = 'Customer_ID'
+    print('num of loans')
+    clean_numerical_field(df, group_by, column_name)
+
+    column_name = 'Num_of_Delayed_Payment'
+    group_by = 'Customer_ID'
+    clean_numerical_field(df, group_by, column_name, strip='_', datatype='float')
+
+    column_name = 'Num_Credit_Inquiries'
+    group_by = 'Customer_ID'
+    clean_numerical_field(df, group_by, column_name)
+
+    column_name = 'Outstanding_Debt'
+    group_by = 'Customer_ID'
+    clean_numerical_field(df, group_by, column_name, strip='_', datatype=float)
+
+    df['Credit_History_Age'] = df['Credit_History_Age'].apply(lambda x: Month_Converter(x)).astype(float)
+    column_name = 'Credit_History_Age'
+    group_by = 'Customer_ID'
+    clean_numerical_field(df, group_by, column_name, datatype=float)
+
+    column_name = 'Amount_invested_monthly'
+    group_by = 'Customer_ID'
+    clean_numerical_field(df, group_by, column_name, datatype=float, strip='_')
+
+    df['Type_of_Loan'].replace([np.NaN], 'Not Specified', inplace=True)
     return df
+
+
+
